1b3f46ca4222e77729c4654660e251f0faf711a7
Darius & Scott Chou <pair+datmar.scottychou@devbootcamp.com>
2016-01-08 15:23:57 -0500

    Fix naming conventions options/optionsquestions

--- db/migrate/20160108122233_create_options.rb
+++ db/migrate/20160108122233_create_options.rb
@@ -1,9 +1,9 @@
 class CreateOptions < ActiveRecord::Migration
   def change
-    create_table :option do |t|
+    create_table :options do |t|
       t.string :text, null: false
 
-      t.timestamps, null: false
+      t.timestamps null: false
     end
   end
 end
