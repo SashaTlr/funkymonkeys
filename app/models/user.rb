4c4805907a0ac2c9e76ebf2f2843e8f8b6c7f5d3
lateesha <lnthomas02@gmail.com>
2016-01-09 11:36:52 -0500

    add validations and associations on user model, create sessions helper file to look for a current user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,19 @@
 class User < ActiveRecord::Base
+  include BCrypt
 
+  has_many :surveys
+  has_many :completed_surveys
+
+  validates :username, length: {maximum: 50}, presence: true, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
 end
+
