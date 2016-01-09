3a9facc0291c915b3ac9f9630337b9900f502353
Sasha <email": null>
2016-01-08 19:01:23 -0500

    Add database migrations

--- app/controllers/completed_surveys_controller.rb
+++ app/controllers/completed_surveys_controller.rb
@@ -1,17 +1,17 @@
-get 'survey/:id/completed_survey/new' do
-  @survey = Survey.find(params[:id])
-  erb :'completed_survey/new'
-end
+# get 'survey/:id/completed_survey/new' do
+#   @survey = Survey.find(params[:id])
+#   erb :'completed_survey/new'
+# end
 
 
-post 'completed_surveys' do
-  @new_survey = CompletedSurvey.create(params[:completed_survey])
-  redirect "completed_surveys/#{@new_survey.id}/survey_responses/new"
-end
+# post 'completed_surveys' do
+#   @new_survey = CompletedSurvey.create(params[:completed_survey])
+#   redirect "completed_surveys/#{@new_survey.id}/survey_responses/new"
+# end
 
-get 'completed_surveys/:id/survey_responses/new' do
-  #create instance variable from method call for next question
-  erb: '/options_questions/show'
-end
+# get 'completed_surveys/:id/survey_responses/new' do
+#   #create instance variable from method call for next question
+#   erb: '/options_questions/show'
+# end
 
 
