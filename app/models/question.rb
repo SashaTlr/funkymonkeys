f338baada31186d96c14284c6cf652355f0bcb4f
Sasha <email": null>
2016-01-09 13:45:15 -0500

    Complete migrations and models for survey_responses and survey_users, update seed file, change user_id to creator_id in create_survey migration

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,12 @@
 class Question < ActiveRecord::Base
-  validates :text, presence: true, length: {maximum: 512}
-  validates :survey_id, presence: true
 
   belongs_to :survey
   has_many :option_questions
   has_many :options, through: :option_questions
   has_many :survey_responses
+
+  validates :text, presence: true, length: {maximum: 512}
+  validates :survey_id, presence: true
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,22 @@
 2.times {
-  survey = Survey.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence)
+  user = User.create(username:Faker::Internet.user_name , password_hash: Faker::Internet.password(8))
+  survey = Survey.create(name: Faker::Lorem.word, description: Faker::Lorem.sentence, creator_id: user.id)
   4.times {
     question = Question.create(text: (Faker::Lorem.sentence + "?"), survey: survey)
     4.times {
       option = Option.create(text: Faker::Lorem.word)
       option_question = OptionQuestion.create(option: option, question: question)
+
     }
   }
 }
 
 
+User.all.each do |user|
+  Survey.all.each do |survey|
+    survey_taker = SurveyUser.create(survey_id: survey.id, user_id: user.id)
+    survey.questions.each do |question|
+      survey_responses = SurveyResponse.create(survey_id: survey.id, user_id: user.id, question_id: question.id, option_id: rand(1..4))
+    end
+  end
+end
