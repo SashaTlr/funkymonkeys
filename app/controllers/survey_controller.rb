0110ecee7140bba8c077243cc0c8fb500e88e31d
Sasha <email": null>
2016-01-09 15:32:22 -0500

    Write survey controller routes

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,31 +1,38 @@
 get '/surveys' do
   @surveys = Survey.all
-  erb :"/surveys/index"
+  erb :"surveys/index"
 end
 
 get '/surveys/new' do
-  erb :"/surveys/new"
+  erb :"surveys/new"
 end
 
+#create a new survey
 post '/surveys' do
   @survey = Survey.new(params[:survey])
   if @survey.save
     redirect "/surveys/#{@survey.id}"
   else
     @errors = @survey.errors.full_messages
-    erb :"/surveys/new"
+    erb :"surveys/new"
   end
 end
 
-get '/surveys/:id' do
-  @survey = Survey.find(params[:id])
-  erb :"/surveys/show"
+# #show survey and create questions and answers
+# get '/surveys/:id' do
+#   @survey = Survey.find(params[:id])
+#   if logged_in? && current_user.id = @survey.creator_id
+#     erb :"/surveys/show"
+#   end
+# end
+
+
+get '/surveys/:id/edit' do
+ @survey = Survey.find(params[:id])
+ erb :"surveys/edit"
 end
 
- # get 'survey/:id/edit' do
- #   @survey = Survey.find(params[:id])
-
- #   erb :"/surveys/edit"
-
- # end
-
+get 'surveys/:id/complete_surveys/new' do
+  @survey = Survey.find_by(id: params[:id])
+  erb :"completed_surveys/new"
+end
