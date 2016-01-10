9df3bf8d6b06d47d4cd3847ee6e2cfe535e52bc6
Sasha Tlr <sashatailor@gmail.com>
2016-01-10 01:24:04 -0500

    Add edit functionality to existing questions and answers in a survey. Add JS to add row feature in question and answer creation.

--- app/controllers/option_controller.rb
+++ app/controllers/option_controller.rb
@@ -1,17 +1,15 @@
 
 
 put '/surveys/:id/options' do
-
   survey = Survey.find(params[:id])
-  binding.pry
-  #put in logic to find the right option
-  #need a hidden value in the options form - line 6, options/_edit.erb
-  #option = survey.questions.options.update_attributes(text: params[:options])
-
-  redirect '/surveys/#{survey.id}'
-
+  question = survey.questions.find_by(id: params[:question][:id])
+  question.options.find_by(id: params[:options][:id]).destroy
+  question.options << Option.find_or_create_by(text: params[:options][:text])
+  redirect "/surveys/#{survey.id}"
 end
 
 delete '/surveys/:id/options' do
-
+  survey_id = params[:id]
+  question.options.find_by(id: params[:options][:id]).destroy
+  redirect "/surveys/#{survey_id}"
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
+  $("#add-row").click(function(){
 
+    var option_count = $(".question-option-input").length;
+
+    $("#options-container").append("<label for = \"question-option-input\" >Question Response Option:</label> <input class = \"question-option-input\" name = \"options[" + option_count + 1 + "]\" type = \"text\"><br>");
+  });
 });
