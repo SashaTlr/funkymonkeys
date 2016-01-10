6d1b1b554d2cd1a4d0e092b2e7399dcf7eafca01
Lateesha Thomas <lateesha@devbootcamp.com>
2016-01-10 16:26:28 -0500

    fix merge conflicts

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -12,41 +12,29 @@ post '/surveys/:id/question' do
   redirect "/surveys/#{@survey.id}"
 end
 
-<<<<<<< HEAD
-put '/surveys/:id/question/edit' do
-  survey = Survey.find(params[:id])
-  question = Question.find_by(id: params[:question][:id])
+get '/question/:id/edit' do
+  @question = Question.find(params[:id])
+    erb :"questions/_edit"
+end
+
+
+put '/question/:id' do
+
+  question = Question.find(params[:id])
   question.update_attributes(params[:question])
 
   question.delete_options
 
   params[:options].each do |option|
     question.options << Option.find_or_create_by(text: option[1])
-=======
-get '/question/:id/edit' do
-  @question = Question.find(params[:id])
-    erb :"questions/_edit"
-end
-
-put '/question/:id' do
-  question = Question.find(params[:id])
-  question.assign_attributes(params[:question])
-  if !question.save
-    @errors = question.errors.full_messages
->>>>>>> 84c3be05a3293b63fb3f8c09c4d88fa7fd8d933e
   end
+
   redirect "/surveys/#{question.survey_id}"
 end
 
-<<<<<<< HEAD
-delete '/surveys/:id/question' do
-  survey_id = params[:id]
-  question = Question.find_by(id: params[:question][:id])
-=======
 delete '/question/:id' do
   question = Question.find(params[:id])
   survey = question.survey
->>>>>>> 84c3be05a3293b63fb3f8c09c4d88fa7fd8d933e
   question.destroy
   redirect "/surveys/#{survey.id}"
 end
